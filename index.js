6a4c1de3b9cbd61fd81c59bb17383827e62c9e09
Pravar Anu <pravaranu6@gmail.com>
2023-02-19 22:12:10 +0530

    Done with almost work

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-document.addEventListener("DOMContentLoaded", ()=>{
+document.addEventListener("DOMContentLoaded", () => {
     let hamButton = document.querySelector(".ham-button");
     let collapsibleMenu = document.querySelector(".collapsible-menu");
     let closeIcon = document.querySelector(".close-icon");
 
-    hamButton.addEventListener('click', ()=>{
-        collapsibleMenu.classList.remove("hidden")
+    hamButton.addEventListener("click", () => {
+        collapsibleMenu.classList.remove("hidden");
     });
 
-
-    closeIcon.addEventListener('click', ()=>{
-        collapsibleMenu.classList.add("hidden")
+    closeIcon.addEventListener("click", () => {
+        collapsibleMenu.classList.add("hidden");
     });
 
-})
+});
