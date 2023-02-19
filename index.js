fd2b105c297a04cb7a2e5875be9c1f8a43a7c0b7
Pravar Anu <pravaranu6@gmail.com>
2023-02-19 11:27:43 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+    let hamButton = document.querySelector(".ham-button");
+    let collapsibleMenu = document.querySelector(".collapsible-menu");
+    let closeIcon = document.querySelector(".close-icon");
+
+    hamButton.addEventListener('click', ()=>{
+        collapsibleMenu.classList.remove("hidden")
+    });
+
+
+    closeIcon.addEventListener('click', ()=>{
+        collapsibleMenu.classList.add("hidden")
+    });
+
+})
